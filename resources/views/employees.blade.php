2b6bcf3911e682bdacb04742883b934aff0cd688
AhmadRizal <amdrizalc@gmail.com>
2021-10-15 14:04:14 +0700

    first commit

---
+++ resources/views/employees.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Employees List') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="mb-6 w">
+                        <a href="{{ route('employee-create') }}"
+                            class="bg-gray-700 rounded-md px-4 py-2 text-white text-md font-medium">Add Employee
+                            Data</a>
+                    </div>
+                    <table class="w-full table-auto">
+                        <thead>
+                            <tr class="bg-gray-100">
+                                <th class="px-4 py-2 border">No.</th>
+                                <th class="px-4 py-2 border">Name</th>
+                                <th class="px-4 py-2 border">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="px-4 py-2 border">1</td>
+                                <td class="px-4 py-2 border">Ahmad Rizal Imaduddin</td>
+                                <td class="px-4 py-2 border">
+                                    <div class="py-2 inline-flex md:flex justify-start">
+                                        <a href="{{ route('employee-show') }}"
+                                            class="bg-green-300 rounded-md px-2 py-2 mr-2 md:px-3 font-semibold">Show</a>
+                                        <a href="{{ route('employee-edit') }}"
+                                            class="bg-yellow-400 rounded-md px-2 py-2 mr-2 md:px-3 font-semibold">Update</a>
+                                        <a href="#"
+                                            class="bg-red-500 rounded-md px-2 py-2 md:px-3 font-semibold">Delete</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/employees', [EmployeeController::class, 'index'])->name('employees');
+    Route::get('/employees/create', [EmployeeController::class, 'create'])->name('employee-create');
+    Route::get('/employees/id', [EmployeeController::class, 'show'])->name('employee-show');
+    Route::get('/employees/id/edit', [EmployeeController::class, 'edit'])->name('employee-edit');
+});
+
+require __DIR__ . '/auth.php';
